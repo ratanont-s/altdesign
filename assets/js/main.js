d53452cf6c4296c18152457f8f44234bde036cc5
Ratanont Sukapan <ratanont@owndays-tech.com>
2020-12-21 17:08:02 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
 var scroll = new SmoothScroll('[data-scroll]');
+var winWidth = screen.width;
 
 $(function () {
   'use strict'
 
   $('[data-toggle="offcanvas"]').on('click', function () {
     $('.offcanvas-collapse').toggleClass('open')
-  })
+  });
+  if (winWidth < 992){
+    $('.navbar-nav .nav-link').on('click', function(){
+      $('.offcanvas-collapse').toggleClass('open')
+    });
+  }
 });
