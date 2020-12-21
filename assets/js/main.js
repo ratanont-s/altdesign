424f04016f913e12368311eed4f706b762372777
Ratanont Sukapan <ratanont@owndays-tech.com>
2020-12-21 16:41:31 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,9 @@
+var scroll = new SmoothScroll('[data-scroll]');
+
 $(function () {
   'use strict'
 
   $('[data-toggle="offcanvas"]').on('click', function () {
     $('.offcanvas-collapse').toggleClass('open')
   })
-
-     // Add smooth scrolling to all links
-  // $("a").on('click', function(event) {
-  //   if (this.hash !== "") {
-  //     event.preventDefault();
-  //     var hash = this.hash;
-  //     $('html, body').animate({
-  //       scrollTop: $(hash).offset().top
-  //     }, 800, function(){
-  //       window.location.hash = hash;
-  //     });
-  //   }
-  // });
 });
